e5a151525d5f540c929a3c28236b3ac7c85e616f
kAwA1290 <m21037@g.metro-cit.ac.jp>
2024-05-12 17:36:54 +0900

    feat: edit aes.h

--- aes.h
+++ aes.h
@@ -2,7 +2,14 @@
 #define AES_H
 
 #include <iostream>
-
 using namespace std;
+
+typedef unsigned char uint8_t;
+#define BLOCKLEN 128;
+
+class AES {
+public:
+	uint8_t cipher(uint8_t in[BLOCKLEN], uint8_t Nr, uint8_t w[BLOCKLEN]);
+}
 
 #endif
